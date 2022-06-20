ac66da1461fec0bd8f049e67d4604d96a23a3034
Pavithra Sethuraman <pavithrasethuraman92@gmail.com>
2022-06-20 18:46:54 +0800

    make it better

--- connection.js
+++ connection.js
@@ -1,26 +1,20 @@
-const mysql = require("promise-mysql");
+const mysql = require("mysql");
 require("dotenv").config();
 
-var config = {
- 
+var connection = mysql.createConnection({
   port: process.env.DB_PORT,
   host: process.env.HOST,
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  connectionLimit:10,
-};
+});
 
-let connection = mysql.createPool(config);
-
-
-
-// connection.getConnection((error) => {
-//   if (!error) {
-//     console.log("Connected to MySQL Server!!!");
-//   } else {
-//     console.log(error);
-//   }
-// });
+connection.connect((error) => {
+  if (!error) {
+    console.log("Connected to MySQL Server!!!");
+  } else {
+    console.log(error);
+  }
+});
 
 module.exports = connection
