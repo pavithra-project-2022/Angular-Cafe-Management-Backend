e183ab01b4a1fcc2af8ef968693fbd92b2f13342
Pavithra Sethuraman <pavithrasethuraman92@gmail.com>
2022-06-21 04:17:00 +0800

    commited

--- index.js
+++ index.js
@@ -9,19 +9,6 @@ const dashboardRoute = require('./routes/dashboard')
 const app = express()
 
 
-connection.query(`create table bill(
-    id int NOT NULL AUTO_INCREMENT,
-    uuid varchar(200) NOT NULL,
-    name varchar(255) NOT NULL,
-    email varchar(255) NOT NULL,
-    contactNumber varchar(20) NOT NULL,
-    paymentMethod varchar(50) NOT NULL,
-    total int NOT NULL,
-    productDetails JSON DEFAULT NULL,
-    createdBy varchar(255) NOT NULL,
-    primary key(id)
-  );`)
-
 
 app.use(cors());
 app.use(express.urlencoded({extended:true}));
@@ -33,8 +20,4 @@ app.use('/bill',billRoute)
 app.use('/dashboard',dashboardRoute)
 
 
-// module.exports = app;
-
-app.listen(process.env.PORT || 8000,()=>{
-    console.log("Server is running at 8000");
-})
+module.exports = app;
