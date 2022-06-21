9571e0eeda8c8387954dcd8d6c2782e52e24a1d3
Pavithra Sethuraman <pavithrasethuraman92@gmail.com>
2022-06-21 15:36:31 +0800

    commited

--- connection.js
+++ connection.js
@@ -1,20 +1,21 @@
 const mysql = require("mysql");
 require("dotenv").config();
 
-var connection = mysql.createConnection({
+var connection = mysql.createPool({
+  connectionLimit : 10,
   host: "us-cdbr-east-05.cleardb.net",
   user: "b3b61e30cb0fce",
   password: "ba39b317",
   database: "heroku_e1e26118f4942d4",
 });
 
-connection.connect((error) => {
-  if (!error) {
-    console.log("Connected to MySQL Server!!!");
-  } else {
-    console.log(error);
-  }
-});
+// connection.connect((error) => {
+//   if (!error) {
+//     console.log("Connected to MySQL Server!!!");
+//   } else {
+//     console.log(error);
+//   }
+// });
 
 
 
